87ae019b1bfe99177429a63bbef9ac8ef3bd67c9
Alexander Golovatenko <golovatenko@soleynet.soley.io>
2020-08-17 14:00:18 +0200

    Get selection

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ miro.onReady(() => {
           title: 'Some title',
           svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
           onClick: () => {
-            alert('Hello!')
+            miro.board.selection.get().then(exportSelection)
           }
         }
       }
     })
   })
+
+function exportSelection(selection) {
+    alert(selection.length)
+}
